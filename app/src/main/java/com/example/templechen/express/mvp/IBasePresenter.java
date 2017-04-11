e182dbf99528290fe51d0a24c1a4e1addfe91eca
templechen <templechen@qiyi.com>
2017-04-11 14:29:14 +0800

    base

--- app/src/main/java/com/example/templechen/express/mvp/IBasePresenter.java
+++ app/src/main/java/com/example/templechen/express/mvp/IBasePresenter.java
@@ -4,5 +4,6 @@ package com.example.templechen.express.mvp;
  * Created by templechen on 2017/4/1.
  */
 
-public interface BasePresenter {
+public interface IBasePresenter {
+
 }
--- app/src/main/java/com/example/templechen/express/mvp/IBaseView.java
+++ app/src/main/java/com/example/templechen/express/mvp/IBaseView.java
@@ -4,5 +4,8 @@ package com.example.templechen.express.mvp;
  * Created by templechen on 2017/4/1.
  */
 
-public interface BaseView {
+public interface IBaseView<T> {
+
+    void setPresenter(T presenter);
+
 }
